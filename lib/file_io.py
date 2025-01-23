19ddf59c0f715410e51aa2ddd721279e6882136f
zahrafeisal <zahrafeisal3@gmail.com>
2025-01-23 19:02:33 +0300

    file I/O

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,14 @@
 def write_file(file_name, file_content):
+    with open(f'{file_name}.txt', mode = 'w', encoding = 'utf-8') as file_name:
+        file_name.write(file_content)
     pass
 
 def append_file(file_name, append_content):
+    with open(f'{file_name}.txt', mode = 'a', encoding = 'utf-8') as file_name:
+        file_name.write(append_content)
     pass
 
 def read_file(file_name):
+    with open(f'{file_name}.txt', encoding = 'utf-8') as file_name:
+        return file_name.read()
     pass
